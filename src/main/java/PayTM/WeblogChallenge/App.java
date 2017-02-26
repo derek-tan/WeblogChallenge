6f55a29e251e100f1a7c83cf7ff7e5d6165c7894
Derek Tan <guoctan@gmail.com>
2017-02-26 16:36:13 -0500

    split into functions

--- src/main/java/PayTM/WeblogChallenge/App.java
+++ src/main/java/PayTM/WeblogChallenge/App.java
@@ -1,8 +1,6 @@
 package PayTM.WeblogChallenge;
 
-import java.io.Serializable;
 import java.text.SimpleDateFormat;
-
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaRDD;
@@ -11,6 +9,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import com.google.common.annotations.VisibleForTesting;
+
 public class App 
 {
 	private final static int TIMESTAMP_INDEX = 0;
@@ -18,7 +18,12 @@ public class App
 	private final static int LB_STATUS_CODE_INDEX = 7;
 	private final static int BACKEND_STATUS_CODE_INDEX = 8;
 	private final static int REQUEST_URL_INDEX = 12;
-
+	
+	private final static String LogEntryView = "LogEntry";
+	private final static String NewSessionLogEntryView = "NewSessionLogEntry";
+	private final static String SessionizedLogEntryView = "SessionizedLogEntryView";
+	private final static String SessionTimeLogEntryView = "SessionTimeLogEntryView";
+	
     public static void main( String[] args ) throws ClassNotFoundException
     {
         if (args.length != 1) {
@@ -32,9 +37,48 @@ public class App
         		.appName("Weblog Challenge")
         		.getOrCreate();
         
-        JavaRDD<String> logEntryStringRDD = spark.read().textFile(logFile).javaRDD();
+        Dataset<Row> logEntryDF = init(spark, logFile);
+        
+        // Register logEntryDF as a temporary view
+        logEntryDF.createOrReplaceTempView(LogEntryView);
 
-        // ETL
+        // Assume a session duration is 15 mins (15 * 60 * 1000 = 900000)
+        Dataset<Row> newSessionDF = createNewSessionColDF(spark, LogEntryView, "900000");
+        
+        // Register newSessionDF as a temporary view
+        newSessionDF.createOrReplaceTempView(NewSessionLogEntryView);
+        
+        // Q1 - Sessionize:
+        Dataset<Row> sessionizedDF = createSessionIDColDF(spark, NewSessionLogEntryView);
+        
+        // Register sessionizedDF as a temporary view
+        sessionizedDF.createOrReplaceTempView(SessionizedLogEntryView);
+        System.out.println("Q1 - Sessionize:");
+        sessionizedDF.show(100);
+        
+        Dataset<Row> sessionTimeDF = findDurationPerSessionDF(spark, SessionizedLogEntryView);
+        
+        // Register sessionTimeDF as a temporary view
+        sessionTimeDF.createOrReplaceTempView(SessionTimeLogEntryView);
+        
+        // Q2 - Avg Session Time:
+        findAvgSessionTimeDF(spark, SessionTimeLogEntryView).show();
+        
+        // Q3 - Unique URL Visits per Session
+        System.out.println("Q3 - Unique Visits per Session:");
+        findUniqueVisitsPerSessionDF(spark, SessionizedLogEntryView).show(100);
+        
+        // Q4 - The Most Engaged Users
+        System.out.println("Q4 - The Most Engaged Users:");
+        findMostEngagedUsersDF(spark, SessionTimeLogEntryView).show(100);
+    }
+    
+    /*
+     * Extract, Transform, Load
+     */
+    @VisibleForTesting
+    static Dataset<Row> init(SparkSession spark, String logFile) {
+    	JavaRDD<String> logEntryStringRDD = spark.read().textFile(logFile).javaRDD();
         JavaRDD<LogEntry> logEntryRDD = logEntryStringRDD.map(new Function<String, LogEntry>() {
 			public LogEntry call(String line) throws Exception {
 				String[] attrs = line.split(" ");
@@ -49,7 +93,7 @@ public class App
 			}
         }).filter(new Function<LogEntry, Boolean>() {
 			public Boolean call(LogEntry entry) throws Exception {
-				// Assumption 1 - non-200 code requests are not considered as valid hits
+				// Assumption 1 - non-200 code requests are not considered valid hits
 				// Assumption 2 - static resource requests are not page visits/valid hits
 				// Assumption 3 - all static resources are in the path /offer/*
 				if (entry.getBackendCode() != 200 || entry.getLbCode() != 200) {
@@ -62,48 +106,47 @@ public class App
 			}  	
         });
         Dataset<Row> logEntryDF = spark.createDataFrame(logEntryRDD, LogEntry.class);
-        // Register logEntryDF as a temporary view
-        logEntryDF.createOrReplaceTempView("LogEntry");
-
-        // Assume a session duration is 15 mins (15 * 60 * 1000 = 900000)
+        return logEntryDF;
+    }
+    @VisibleForTesting
+    static Dataset<Row> createNewSessionColDF(SparkSession spark, String logEntryView, String sessionDuration) {
         // newSession [boolean] = LAG(timestamp) = null or timestamp - LAG(timestamp) > 900000 THEN 1 ELSE 0
-        String newSessionSelection = "CASE WHEN (LAG(timestamp) OVER (PARTITION BY clientIP ORDER BY timestamp) IS NULL) OR (timestamp - LAG(timestamp) OVER (PARTITION BY clientIP ORDER BY timestamp) > 900000) THEN 1 ELSE 0 END AS newSession";
-        String newSessionQuery = "SELECT timestamp, clientIP, requestURL, " + newSessionSelection + " FROM LogEntry";
+    	// 1 indicates the starting of a new session
+        String newSessionSelection = "CASE WHEN (LAG(timestamp) OVER (PARTITION BY clientIP ORDER BY timestamp) IS NULL) OR (timestamp - LAG(timestamp) OVER (PARTITION BY clientIP ORDER BY timestamp) > " + sessionDuration + ") THEN 1 ELSE 0 END AS newSession";
+        String newSessionQuery = "SELECT timestamp, clientIP, requestURL, " + newSessionSelection + " FROM " + logEntryView;
         Dataset<Row> newSessionDF = spark.sql(newSessionQuery);
-        
-        // Register newSessionDF as a temporary view
-        newSessionDF.createOrReplaceTempView("NewSessionLogEntry");
-        
-        // Q1 - Sessionize:
-        // sessionID = cumulative SUM(newSession)
+        return newSessionDF;
+    }
+    @VisibleForTesting
+    static Dataset<Row> createSessionIDColDF(SparkSession spark, String newSessionLogEntryView) {
+    	// sessionID = cumulative SUM(newSession)
         // clientIP + sessionID can uniquely identify a session.
-        String sessionizationQuery = "SELECT clientIP, SUM(newSession) OVER (PARTITION BY clientIP ORDER BY timestamp) AS sessionID, requestURL, timestamp FROM NewSessionLogEntry";
+        String sessionizationQuery = "SELECT clientIP, SUM(newSession) OVER (PARTITION BY clientIP ORDER BY timestamp) AS sessionID, requestURL, timestamp FROM " + newSessionLogEntryView;
         Dataset<Row> sessionizationDF = spark.sql(sessionizationQuery);
-        // Register sessionizationDF as a temporary view
-        sessionizationDF.createOrReplaceTempView("SessionizedLogEntry");
-        String sessionTimeQuery = "SELECT clientIP, sessionID, (MAX(timestamp) - MIN(timestamp)) as sessionTime FROM SessionizedLogEntry GROUP BY sessionID, clientIP";
+        return sessionizationDF;
+    }
+    @VisibleForTesting
+    static Dataset<Row> findDurationPerSessionDF(SparkSession spark, String sessionizedLogEntryView) {
+        String sessionTimeQuery = "SELECT clientIP, sessionID, (MAX(timestamp) - MIN(timestamp)) as sessionTime FROM " + sessionizedLogEntryView + " GROUP BY sessionID, clientIP";
         Dataset<Row> sessionTimeDF = spark.sql(sessionTimeQuery);
-        
-        // Register sessionTimeDiffDF as a temporary view
-        sessionTimeDF.createOrReplaceTempView("SessionTimeLogEntry");
-        System.out.println("Q1 - Sessionize:");
-        sessionTimeDF.show(100);
-        
-        // Q2 - Avg Session Time:
-        System.out.println("Q2 - Avg Session Time:");
-        String avgSessionTimeQuery = "SELECT AVG(sessionTime) FROM SessionTimeLogEntry";
-        spark.sql(avgSessionTimeQuery).show();
-        
-        // Q3 - Unique URL Visits per Session
-        System.out.println("Q3 - Unique URL Visits per Session:");
-        String uniqueVisitPerSessionQuery = "SELECT clientIP, sessionID, COUNT(DISTINCT requestURL) AS uniqueVisits FROM SessionizedLogEntry GROUP BY sessionID, clientIP";
+        return sessionTimeDF;
+    }
+    @VisibleForTesting
+    static Dataset<Row> findAvgSessionTimeDF(SparkSession spark, String sessionTimeLogEntryView) {
+    	System.out.println("Q2 - Avg Session Time:");
+        String avgSessionTimeQuery = "SELECT AVG(sessionTime) FROM " + sessionTimeLogEntryView;
+        return spark.sql(avgSessionTimeQuery);
+    }
+    @VisibleForTesting
+    static Dataset<Row> findUniqueVisitsPerSessionDF(SparkSession spark, String sessionizedLogEntryView) {
+    	String uniqueVisitPerSessionQuery = "SELECT clientIP, sessionID, COUNT(DISTINCT requestURL) AS uniqueVisits FROM " + sessionizedLogEntryView + " GROUP BY sessionID, clientIP";
         Dataset<Row> uniqueVisitPerSessionDF = spark.sql(uniqueVisitPerSessionQuery);
-        uniqueVisitPerSessionDF.show(100);
-        
-        // Q4 - The Most Engaged Users
-        System.out.println("Q4 - The Most Engaged Users:");
-        String mostEngagedUsersQuery = "SELECT clientIP, SUM(SessionTime) AS engagedTime FROM SessionTimeLogEntry GROUP BY clientIP SORT BY engagedTime DESC";
+        return uniqueVisitPerSessionDF;
+    }
+    @VisibleForTesting
+    static Dataset<Row> findMostEngagedUsersDF(SparkSession spark, String sessionTimeLogEntryView) {
+    	String mostEngagedUsersQuery = "SELECT clientIP, SUM(SessionTime) AS engagedTime FROM " + sessionTimeLogEntryView + " GROUP BY clientIP SORT BY engagedTime DESC";
         Dataset<Row> mostEngagedUsersDF = spark.sql(mostEngagedUsersQuery);
-        mostEngagedUsersDF.show(100);
+        return mostEngagedUsersDF;
     }
 }
